5a8a45feac288bb1aa853afa6bbefe06c1541ccb
AliceCengal <cengallut@gmail.com>
2014-04-23 03:07:28 -0500

    Converted to Scala

---
+++ src/com/marsupial/wallaby/MainActivity.scala
@@ -0,0 +1,16 @@
+package com.marsupial.wallaby
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity extends Activity {
+
+    /**
+     * Called when the activity is first created.
+     */
+    override def onCreate(saved: Bundle) {
+        super.onCreate(saved)
+        setContentView(R.layout.main)
+    }
+
+}
