25c70f2ff7bbf15050aac42036f184a62a93661f
AliceCengal <cengallut@gmail.com>
2014-04-23 06:11:36 -0500

    fixes

--- src/com/marsupial/wallaby/MainActivity.scala
+++ src/com/marsupial/wallaby/MainActivity.scala
@@ -5,12 +5,12 @@ import android.os.Bundle
 
 class MainActivity extends Activity {
 
-    /**
-     * Called when the activity is first created.
-     */
-    override def onCreate(saved: Bundle) {
-        super.onCreate(saved)
-        setContentView(R.layout.main)
-    }
+  /**
+   * Called when the activity is first created.
+   */
+  override def onCreate(saved: Bundle) {
+    super.onCreate(saved)
+    setContentView(R.layout.main)
+  }
 
 }
